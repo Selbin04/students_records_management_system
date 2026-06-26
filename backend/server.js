9966dba6ed01cf532e108295adaea36ee64c7ce7
Selbin04 <selbinthomas0405@gmail.com>
2026-06-26 19:32:49 +0530

    feat: established mongodb connection using mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import 'dotenv/config';
+import connectDB from "./database/db.js";
 
 
 const app = express();
@@ -9,6 +10,16 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+const startServer = async () => {
+    try{
+        await connectDB();
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    }catch(e){
+        console.error(e);
+        process.exit(1);
+    }
+}
+
+startServer();
