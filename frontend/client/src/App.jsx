a5e53c0d7e9637f1c6ac06ff96a75baf86f8c84f
selbin04 <selbinthomas0405@gmail.com>
2026-08-09 16:23:58 +0530

    feat: add student input details form layout and fields

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
-
+import './App.css';
 function App() {
+
+  //const [data, setData] = useState(null);
+
+
+  //const fetchData = async () => {}
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>ABCD School</h1>
+      <h3>Student Management System</h3>
+      <form className='form'>
+        <input className='input' type="text" placeholder="Name" />
+        <input className='input' type="email" placeholder="Email" />
+        <input className='input' type="text" placeholder="Father's name" />
+        <input className='input' type="number" placeholder="Mobile number" />
+        <button className='button' type="submit">Submit</button>
+      </form>
+      <h3>Students List</h3>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+      </table>
     </div>
   )
 }
